a7deb60ade4b6764502396cb67d2f50a8e5bb455
Allen Sanders <asanders@gamuttechnologysvcs.com>
2025-10-15 19:58:35 -0400

    updated code

--- domain/src/main/java/com/example/druginteractions/domain/ports/DrugInteractionRepository.java
+++ domain/src/main/java/com/example/druginteractions/domain/ports/DrugInteractionRepository.java
@@ -2,8 +2,11 @@ package com.example.druginteractions.domain.ports;
 
 import com.example.druginteractions.domain.model.DrugPair;
 import com.example.druginteractions.domain.model.InteractionNote;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface DrugInteractionRepository {
   Optional<InteractionNote> find(DrugPair pair);
 
